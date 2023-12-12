fc9af57e524a34eaf6b1161e56c0e49211b12c37
unknown <green.sneha@gmail.com>
2023-12-11 19:03:30 -0600

    updated order page based on review comment

--- src/main/java/com/qa/opencart/pages/Order.java
+++ src/main/java/com/qa/opencart/pages/Order.java
@@ -1,7 +1,18 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class Order {
 	
-	String locator ="";
+	By locator =By.id("order");
+	By price =By.id("price");
+	
+	public void order() {
+		System.out.println("order..");
+	}
+
+	public void price() {
+		System.out.println("price..");
+	}
 
 }
